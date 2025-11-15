6012eab27bf2942891232e144e4f5a26c2915e8c
zma8 <zainabmoosa880@gmail.com>
2025-11-15 16:01:25 +0300

    completed the connection in socket.js

--- src/socket.js
+++ src/socket.js
@@ -13,3 +13,17 @@ socket.on('connect', () => {
   console.log('✅ Connected to server:', socket.id);
 });
 
+socket.on('connect',()=>{
+    console.log('connected to server',socket.id);
+});
+
+socket.on('disconnected',()=>{
+    console.log('Disconnected from server');
+});
+
+socket.on('connect_error',(error)=>{
+    console.error('Connection error:' ,error);
+});
+
+export default socket;
+
