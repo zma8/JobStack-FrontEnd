702f563fb4b3103058cba16f6d6bf5fb24e680c9
zma8 <zainabmoosa880@gmail.com>
2025-11-16 12:42:22 +0300

    creating the chat page

---
+++ src/components/ChatPage.jsx
@@ -0,0 +1,5 @@
+import { useState, useContext } from 'react';
+import ChatList from '../Chat/ChatList';
+import ChatWindow from '../Chat/ChatWindow';
+import { UserContext } from '../contexts/UserContext';
+
